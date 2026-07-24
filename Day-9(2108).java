ca9764fedd04ccbd7ea1ad9201ad43a6ab3e6812
jaiseelan7464-arch <jaiseelan7464@gmail.com>
2026-07-24 08:42:52 +0530

    Implement firstPalindrome method in Solution class

---
+++ Day-9(2108).java
@@ -0,0 +1,26 @@
+QN(03) LEETCODE(2108)
+
+class Solution {
+    public String firstPalindrome(String[] words) {
+        
+        for (String word : words)
+        {
+            if ( ispalindrome(word)){
+                return word;
+            }
+        }
+            return"";
+        }
+        public boolean ispalindrome(String s){
+            int left = 0;
+            int right = s.length()-1;
+            while(left<right){
+                if (s.charAt(left) != s.charAt(right)){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
